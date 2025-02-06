86e5f99ccc4086867119ba94c9c09c9d8c33911a
siggu <m0_w_0m@naver.com>
2025-02-06 13:42:08 +0900

    feat: DateDisplay component

--- app/meeting/list/components/Card.tsx
+++ app/meeting/list/components/Card.tsx
@@ -1,9 +1,12 @@
 import { ReactNode } from 'react';
 import Title from './Title';
+import DateDisplay from './DateDisplay';
 
 const Card = ({ children }: { children: ReactNode }) => {
   return { children };
 };
 
 Card.Title = Title;
+Card.DateDisplay = DateDisplay;
+
 export default Card;
