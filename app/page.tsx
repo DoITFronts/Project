9ef1129f0ebaf527f02cd68f7be97961e739ff6a
Choi Seung-eun <138289674+xeunnie@users.noreply.github.com>
2025-02-11 11:48:58 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import RootLayout from '@/app/layout';
-
 export default function Page() {
   return (
-    <RootLayout>
-      <div className="flex flex-col gap-2.5 bg-amber-50 p-10">page components</div>
-    </RootLayout>
+    <div className="flex flex-col gap-2.5 bg-amber-50 p-10">
+      page components
+    </div>
   );
 }
