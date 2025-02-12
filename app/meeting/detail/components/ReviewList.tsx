3f0eec11d3a58de00882126c3b052ad74b4f2f21
chloe_choi <plumxeun@gmail.com>
2025-02-12 16:37:18 +0900

    DOIT-57 feat: 상세조회 기능 구현

--- app/meeting/detail/components/ReviewList.tsx
+++ app/meeting/detail/components/ReviewList.tsx
@@ -5,7 +5,7 @@ export default async function ReviewList() {
     <div className="mt-6 rounded-lg bg-white p-4 shadow-md">
       <h3 className="text-lg font-bold">이용자들의 후기</h3>
       <div className="mt-4 space-y-4">
-        {reviews.map((review:any) => (
+        {reviews.map((review: any) => (
           <div key={review.id} className="rounded-lg border bg-gray-100 p-4">
             <div className="flex items-center">
               <span className="text-orange-500">⭐⭐⭐⭐⭐</span>
