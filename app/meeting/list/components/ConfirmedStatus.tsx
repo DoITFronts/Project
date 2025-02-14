56cd3b2c64dae21999679dbd5a312c583467c8e9
siggu <m0_w_0m@naver.com>
2025-02-14 15:21:55 +0900

    fix: CheckIcon 색 변경
    - ConfirmedStatus 컴포넌트에도 적용

--- app/meeting/list/components/ConfirmedStatus.tsx
+++ app/meeting/list/components/ConfirmedStatus.tsx
@@ -5,7 +5,7 @@ function ConfirmedStatus({ isCompleted }: { isCompleted: boolean }) {
 
   return (
     <div className="inline-flex h-6 items-center justify-start gap-1">
-      <CheckIcon variant="filled-orange" />
+      <CheckIcon variant="filled-yellow" size="lg" />
       <div className="font-['Pretendard'] text-base font-semibold leading-tight text-[#ffd500]">
         개설확정
       </div>
