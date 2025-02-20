a1da9e18a73252c94cddb09ccd10238c589ef666
siggu <m0_w_0m@naver.com>
2025-02-20 09:12:00 +0900

    feat: msw handler 추가

--- api/mocks/handlers.ts
+++ api/mocks/handlers.ts
@@ -3,6 +3,8 @@ import { rest } from 'msw';
 
 import mockMeetingDetail from '@/api/data/mockMeetingDetail';
 
+import mockMeetings from '../data/mockMeetings';
+
 const handlers = [
   rest.get('/api/meeting/detail/:id', (req, res, ctx) => {
     const { id } = req.params;
@@ -14,6 +16,27 @@ const handlers = [
     console.log('Received ID:', id);
 
     return res(ctx.status(200), ctx.json({ ...mockMeetingDetail, id }));
+  }),
+
+  rest.get('/api/meeting/list', (req, res, ctx) => {
+    const tab = req.url.searchParams.get('tab') || '전체';
+    const location1 = req.url.searchParams.get('location_1') || '지역 전체';
+    const location2 = req.url.searchParams.get('location_2') || '동 전체';
+    const date = req.url.searchParams.get('date') || '';
+
+    // 필터링 로직
+    const filteredMeetings = mockMeetings.filter((meeting) => {
+      const matchesTab = tab === '전체' || meeting.category === tab;
+      const matchesLocation1 =
+        location1 === '지역 전체' || meeting.location.region_1depth_name === location1;
+      const matchesLocation2 =
+        location2 === '동 전체' || meeting.location.region_2depth_name === location2;
+      const matchesDate = date === '' || meeting.dateTime.startsWith(date);
+
+      return matchesTab && matchesLocation1 && matchesLocation2 && matchesDate;
+    });
+
+    return res(ctx.status(200), ctx.json(filteredMeetings));
   }),
 ];
 
