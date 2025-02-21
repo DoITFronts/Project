a33d2d7b93179a99b9dab7cdc95552809a0c299f
soyoung <ssh0040233@gmail.com>
2025-02-21 11:29:53 +0900

    feat: instance폴더에 axios.ts 추가

---
+++ api/instance/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
+
+// 요청마다 `Authorization` 헤더를 동적으로 추가 (인터셉트 활용)
+// ~로그인에선 필요없는 부분~
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default axiosInstance;
