b8bbb8a650f00d91f870f846b35c81853cf0ee2e
soyoung <ssh0040233@gmail.com>
2025-02-21 16:21:44 +0900

    feat: 회원가입 성공시 Toast 띄워주기

--- app/user/signup/page.tsx
+++ app/user/signup/page.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { signupUser } from '@/api/auth';
 import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
 
 export default function Signup() {
   const router = useRouter();
@@ -22,6 +23,11 @@ export default function Signup() {
       const response = await signupUser(data);
       console.log('회원가입 응답:', response);
       setSuccess('회원가입 성공!');
+      //TODO: Toast 디자인 변경
+      toast.success('회원가입이 완료되었습니다.', {
+        hideProgressBar: true,
+        autoClose: 900,
+      });
       router.push('/user/signin');
     } catch (err: any) {
       setError('회원가입에 실패했습니다. 다시 시도해 주세요.');
