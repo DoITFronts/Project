7005e561d3bb4f0f1924e521c0114188f1d1551c
siggu <m0_w_0m@naver.com>
2025-02-24 18:29:50 +0900

    fix: searchParams 타입 에러 해결

--- app/meeting/list/page.tsx
+++ app/meeting/list/page.tsx
@@ -2,18 +2,17 @@ import fetchMeeting from '@/api/meeting/fetchMeeting';
 
 import MeetingList from './components/MeetingList';
 
-export default async function Page({
-  searchParams,
-}: {
+interface PageProps {
   searchParams: { [key: string]: string | undefined };
-}) {
+}
+
+export default async function Page({ searchParams }: PageProps) {
   const params = await searchParams;
   const category = params?.category ?? '';
   const location1 = params?.location_1 ?? '';
   const location2 = params?.location_2 ?? '';
   const date = params?.date ? new Date(params.date) : null;
 
-  // 필터 조건을 기반으로 서버에서 초기 데이터 가져오기
   const initialMeetings = await fetchMeeting({
     category,
     location1,
