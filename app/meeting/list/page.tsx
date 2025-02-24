e4f761ba49852610e0b3bcd832280167c3aa1c77
siggu <m0_w_0m@naver.com>
2025-02-24 16:01:41 +0900

    fix: 초기 데이터 가져오기 방식 변경
    - 카테고리/필터링 반영

--- app/meeting/list/page.tsx
+++ app/meeting/list/page.tsx
@@ -2,14 +2,25 @@ import fetchMeeting from '@/api/meeting/fetchMeeting';
 
 import MeetingList from './components/MeetingList';
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const params = await searchParams;
+  const category = params?.category ?? '';
+  const location1 = params?.location_1 ?? '';
+  const location2 = params?.location_2 ?? '';
+  const date = params?.date ? new Date(params.date) : null;
+
+  // 필터 조건을 기반으로 서버에서 초기 데이터 가져오기
   const initialMeetings = await fetchMeeting({
-    category: '전체',
-    location_1: '지역 전체',
-    location_2: '동 전체',
-    date: null,
+    category,
+    location1,
+    location2,
+    date,
     page: 1,
-    limit: 10,
+    per_page: 10,
   });
 
   return <MeetingList initialMeetings={initialMeetings} />;
