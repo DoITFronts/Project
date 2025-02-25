a65550696c2d1e52d9a9bb0ca204f01281dbea38
chloe_choi <plumxeun@gmail.com>
2025-02-25 13:28:58 +0900

    DOIT-73 feat: pwa manifest 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
 import '@/styles/global.css';
 import React from 'react';
-
 import Gnb from '@/components/layout/Gnb';
 
 export const metadata: Metadata = {
   title: '번개팅',
-  description: '같이 달랩은 모임을 위한 서비스입니다.',
+  description: '번개팅은 번개 모임 서비스입니다.',
+  manifest: "/manifest.json"
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
